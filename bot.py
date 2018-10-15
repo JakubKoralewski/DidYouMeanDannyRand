d880c7f15588f7c867342a14e4a1906ffb6af8ed
Jakub Koralewski <jakub.koralewski1@gmail.com>
2018-10-15 18:15:30 +0200

    functions, asyncs

--- bot.py
+++ bot.py
@@ -3,10 +3,141 @@ import os
 import random
 import time
 import re
+import asyncio
 
 import praw
 
-from bot_helper import *
+
+class BreakLoop(Exception):
+    pass
+
+
+async def check_comments(sub):
+    print(f'starting in: {sub}')
+    for comment in sub.stream.comments(pause_after=0):
+        if comment is None:
+            break
+        try:
+            print(f'checking {sub}')
+            await subreddit_search(comment)
+        except BreakLoop:
+            pass
+        await asyncio.sleep(0.1)
+
+
+async def subreddit_search(comment):
+
+    text = comment.body
+    # CHECKS:
+    # Should not reply to yourself.
+    yourself = True if comment.author == my_username else False
+    if yourself:
+        raise BreakLoop
+
+    # Should not reply twice.
+    duplicate = check_if_duplicate(comment.id)
+    if duplicate:
+        raise BreakLoop
+
+    # Delete quotes
+    quotes = re.compile(r'(?<=\n)|(?<=^)>.+\n')
+    text = re.sub(quotes, '', text)
+
+    # Get catch words that are in comment.
+    all_matches = []
+    for catch in catches:
+
+        # Get the parts where someone mentions the Immortal Iron Fist by the list(catches).
+        p = re.compile(
+            r'(\S+\s){0,4}(' + catch + r')\s?(\S+\s){0,4}', re.IGNORECASE)
+        matches = re.finditer(p, text)
+
+        await asyncio.sleep(0.01)
+
+        if matches:
+            for quote in matches:
+                all_matches.append([catch, quote])
+
+    # if no catch then don't bother or if more or equal to three of the matches
+    # All catch words should not be in comment,
+    # because bot should not correct when someone uses the whole catchphrase!
+    save_as_duplicate(comment.id)
+    if not all_matches or len(all_matches) >= 3:
+        print('not all_matches or len(all_matches>=3')
+        print(all_matches)
+        raise BreakLoop
+
+    print(all_matches)
+    print('\n\n', text)
+
+    for match in all_matches:
+
+        catch = match[0]
+        quote = match[1]
+        ai_answer = f"Who is {quote.group(2)}?"
+
+        # Get random answer.
+        i = random.randint(0, len(answers)-1)
+        quote = bold_quote(catch, quote[0])
+        await asyncio.sleep(0.01)
+
+    answer = f"> {quote}\n\n{ai_answer} {answers[i]}"
+    print(f'\nANSWER:\n {answer}')
+
+
+def save_as_duplicate(id):
+    print(f"Saving id:'{id}' as duplicate", end="")
+    with open('commented.txt', 'a+') as commented:
+        print(" to file...")
+        commented.write(id + ',')
+
+
+def check_if_duplicate(id):
+    # if file containing duplicates doesn't exist:
+    # its not a duplicate -> finish execution
+    exists = os.path.exists('commented.txt')
+    if not exists:
+        return False
+
+    with open('commented.txt', 'r') as commented:
+        commented = commented.read().split(',')
+        for otherid in commented:
+            otherid = otherid.strip()
+            if otherid == id:
+                return True
+
+    return False
+
+
+def bold_quote(catch, quote):
+    bolded = f'**{catch}**'
+    quote = re.sub(catch, bolded, quote)
+    return quote
+
+
+def test_comments():
+    with open('testcomments.txt', 'r') as c:
+        comments = c.read().split(',')
+    return comments
+
+
+async def main():
+
+    defenders = reddit.subreddit("Defenders")
+    marvelstudios = reddit.subreddit("marvelstudios")
+
+    await asyncio.gather(
+        check_comments(defenders),
+        check_comments(marvelstudios),)
+
+
+# Log in.
+reddit = praw.Reddit(user_agent=os.environ['USER_AGENT'],
+                     client_id=os.environ['CLIENT_ID'], client_secret=os.environ['CLIENT_SECRET'],
+                     username=os.environ['REDDIT_USERNAME'], password=os.environ['REDDIT_PASSWORD'])
+
+my_username = reddit.user.me()
+print(f"Logged in as {my_username}")
 
 # Get triggers, answers from JSON file
 with open('quotes.json', 'r') as quotes:
@@ -22,95 +153,7 @@ all_answers = quotes['answers']
 answers = all_answers['basic']
 answers_hi = all_answers['hi']
 
-corrects = ["iron fist", "protector of ", "sworn enemy of the hand"]
-corrects_kunlun = ["kunlun","k'un-lun","kun-lun"]
+start = time.time()
+max_run_time = 5
 
-# Log in.
-reddit = praw.Reddit(user_agent=os.environ['USER_AGENT'],
-                     client_id=os.environ['CLIENT_ID'], client_secret=os.environ['CLIENT_SECRET'],
-                     username=os.environ['REDDIT_USERNAME'], password=os.environ['REDDIT_PASSWORD'])
-
-my_username = reddit.user.me()
-print(f"Logged in as {my_username}")
-
-defenders = reddit.subreddit("Defenders")
-
-EXAMPLE_COMMENTS = [
-    """> THIS IS A COMMENT
-
-    NOT TAlking about if""",
-    """alalal al lal las i am talking about the Iron FIST""",
-    """immortal Iron Fist, protector of Kunlun, sworn enemy of the hand""",
-    """ danny rand """,
-    """ SOMETHIN SOMETHING HERE COMES A RANDOM GREATER tahn > and talking more
-
-    gut shit danny rand"""
-
-]
-
-
-for comment in defenders.stream.comments():
-    text = comment.body
-
-
-    # CHECKS:
-
-    # Should not reply to yourself.
-    yourself = False
-    
-    if comment.author == my_username:
-        yourself = True
-
-    duplicate = False
-    # Should not reply twice.
-    duplicate = check_if_duplicate(comment.id)
-    print(f'duplicate: {duplicate}')
-
-    if not yourself and not duplicate:
-        
-        # Delete quotes
-        quotes = re.compile(r'(?<=\n)|(?<=^)>.+\n')
-        text = re.sub(quotes,'',text)
-
-        #print('______________________________AFTERQUOTES\n',text)
-
-        # Get catch words that are in comment.
-        all_matches = []
-        for catch in catches:
-            # Get the parts where someone mentions the Immortal Iron Fist by the list(catches).
-            p = re.compile(r'(\S+\s){0,4}(' + catch + r')\s?(\S+\s){0,4}', re.IGNORECASE)
-            matches = re.finditer(p, text)
-            if matches: 
-                for quote in matches:
-                    all_matches.append([catch,quote])
-        
-        # if no catch then don't bother
-        if not all_matches:
-            continue
-        save_as_duplicate(comment.id)
-        print('\n\n',text)
-
-        # All catch words should not be in comment, 
-        # because bot should not correct when someone uses the whole catchphrase!
-        correctness = re.compile(r'{iron}..?({protector}({k1}|{k2}|{k3}))..?{sworn}'.format(
-            iron=corrects[0],protector=corrects[1],sworn=corrects[2],k1=corrects_kunlun[0],k2=corrects_kunlun[1],k3=corrects_kunlun[2]),re.IGNORECASE)
-        is_correct = True if re.search(correctness,text) else False
-
-        print(f'is_correct: {is_correct}')
-        if not is_correct:
-            for match in all_matches: 
-                catch = match[0]
-                quote = match[1]
-                ai_answer = f"Who is {quote.group(2)}?"
-
-
-                # Get random answer index.
-                i = random.randint(0, len(answers)-1)
-                
-                quote = bold_quote(catch, quote[0])
-                answer = f"""> {quote}
-
-{ai_answer} {answers[i]}
-                """
-                print(f'\nANSWER:\n {answer}')
-
+asyncio.run(main())
--- bot_helper.py
+++ bot_helper.py
@@ -1,33 +1,7 @@
 import os.path
 import re
-
-def save_as_duplicate(id):
-    print(f"Saving id:'{id}' as duplicate",end="")
-    with open('commented.txt','a+') as commented:
-        print(" to file...")
-        commented.write(id+'\n')
+import random
 
 
-def check_if_duplicate(id):
-    # if file containing duplicates doesn't exist:
-    # its not a duplicate -> finish execution
-    exists = os.path.exists('commented.txt')
-    if not exists:
-        return False
-    
-    with open('commented.txt','r') as commented:
-        for otherid in commented:
-            otherid = otherid.strip()
-            if otherid==id:
-                return True
-    
-    return False
 
-
-def bold_quote(catch, quote):
-    #p = re.compile(f'{catch}',re.IGNORECASE)
-    bolded = f'**{catch}**'
-    quote = re.sub(catch, bolded, quote)
-    return quote
-    
 
